182cc08a0cdfb38ab8bf43c6fc07c09951ee4f5d
Trent <trentgrooms3@gmail.com>
2026-04-24 12:13:53 -0500

    Suspense issue

---
+++ app/movie/[id]/page.tsx
@@ -0,0 +1,4 @@
+
+export default function MoviePage({ params }: { params: { id: string } }) {
+    return <div>Movie {params.id}</div>;
+}
---
+++ app/tv/[id]/page.tsx
@@ -0,0 +1,4 @@
+
+export default function TVPage({ params }: { params: { id: string } }) {
+    return <div>TV Show {params.id}</div>;
+}
