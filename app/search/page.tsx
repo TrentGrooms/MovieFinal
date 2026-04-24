4ed32269fd4cfdde75449931b2ef0038ed7ed195
Trent <trentgrooms3@gmail.com>
2026-04-24 11:46:14 -0500

    fixed deployment issues

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,40 +1,10 @@
-"use client"
-import { useSearchParams} from "next/navigation"
-import { useEffect, useState } from "react";
-import MediaCard from "../components/MediaCard"
-import { MediaItem } from "../types/media";
-import NavBar from "../components/NavBar";
-
-
+import { Suspense } from "react";
+import SearchResults from "./SearchResults";
 
 export default function SearchPage() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("query") || "";
-
-  const [results, setResults] = useState<MediaItem[]>([]);
-
-  useEffect(() => {
-    const fetchResults = async () => {
-        if (!query) return;
-        const res = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.NEXT_PUBLIC_API_KEY}&query=${encodeURIComponent(query)}`);
-        const data = await res.json();
-        setResults(data.results);
-    };
-    fetchResults();
-    }, [query]);
-
- 
-
-    return (
-        <div>
-            <NavBar />
-            <h1 className="text-2xl font-bold mb-4">Search Results for "{query}"</h1>
-
-            <div className="grid grid-cols-4 gap-4">
-                {results.map((item) => (
-                    <MediaCard key={item.id} item={item} />
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <SearchResults />
+    </Suspense>
+  );
 }
