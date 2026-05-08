018abd29c2edc190fda9a9ac1d2c8f27cd9a66a9
Trent <trentgrooms3@gmail.com>
2026-05-08 09:55:11 -0500

    fixed prisma issue

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: InstanceType<typeof PrismaClient> };
 
 function createPrismaClient() {
     const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
